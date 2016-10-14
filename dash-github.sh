1daf3e18ac4471b31d55c9ea42f2dbc05550c2ad
Christopher Bruce <christopher.sabine@me.com>
2016-10-13 20:44:50 -0400

    initial commit

---
+++ dash-github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+GETBACK=$(pwd)
+
+package=$(basename $1)
+author=$(basename $(dirname $1))
+
+cd $HOME/Documents/Github
+git clone git@github.com:$(echo $author)/$(echo $package).git
+mv $(echo $package)/README.md $(echo $package).md
+grip $(echo $package).md --export $(echo $package).html
+rm -rf $package
+dashing build mydocs
+open dashing.docset -a Dash
+cd $GETBACK
+
