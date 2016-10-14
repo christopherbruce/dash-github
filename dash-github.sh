b93556e22d011a1ca1fe70b8c0d86bd6acf4affc
Christopher Bruce <christopher.sabine@me.com>
2016-10-13 21:41:14 -0400

    appends github url to readme

--- dash-github.sh
+++ dash-github.sh
@@ -6,10 +6,12 @@ author=$(basename $(dirname $1))
 
 cd $HOME/Documents/Github
 git clone git@github.com:$(echo $author)/$(echo $package).git
-mv $(echo $package)/README.md $(echo $package).md
+shopt -s nocaseglob # makes case insensitive for mv below
+mv $(echo $package)/README.* $(echo $package).md
+echo "https://github.com/$author/$package"|cat - $(echo $package).md > /tmp/out && mv /tmp/out $(echo $package).md
 grip $(echo $package).md --export $(echo $package).html
 rm -rf $package
-# dashing build mydocs
-# open dashing.docset -a Dash
+dashing build mydocs
+open dashing.docset -a Dash
 cd $GETBACK
 
